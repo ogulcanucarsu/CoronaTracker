b43465021abc2e4ee40ac60503a0dbff4e97a0e9
ogulcan <ogulcanucarsu@gmail.com>
2020-04-12 23:49:34 +0300

    created base activity

---
+++ base/app_presentation/src/main/java/org/ucarsu/coronaexample/app/presentation/Constants.kt
@@ -0,0 +1,20 @@
+package org.ucarsu.coronaexample.app.presentation
+
+/*
+ * Core Presentation Ui Constants
+ */
+class Constants private constructor() {
+
+    companion object {
+        /*
+         * Used to notice NO valid Resource id
+         */
+        const val NO_RES = 0
+
+        /*
+         * Profile Picture Dimens
+         */
+        const val DEFAULT_PROFILE_PIC_HEIGHT = 300
+        const val DEFAULT_PROFILE_PIC_WIDTH = 300
+    }
+}
