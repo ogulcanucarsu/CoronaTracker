88b64941530029061bac3e0bc5d403b12937a96f
ogulcan <ogulcanucarsu@gmail.com>
2020-04-25 01:17:50 +0300

    added dashboard's module

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -11,4 +11,10 @@ object Modules {
 
      //Library
      const val core = "$BASE_DIRECTORY:core"
+
+     //Dashboard
+     const val dashBoard = ":dashboard"
+     const val dashBoardData = "$dashBoard:dashboard_data"
+     const val dashBoardDomain = "$dashBoard:dashboard_domain"
+     const val dashBoardPresentation = "$dashBoard:dashboard_presentation"
 }
