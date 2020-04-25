47c5d434d1eddc1bcdc4b19865835daf310b698f
ogulcan <ogulcanucarsu@gmail.com>
2020-04-25 17:01:27 +0300

    refactor

--- app/src/main/java/org/ucarsu/coronaexample/ui/SplashActivity.kt
+++ app/src/main/java/org/ucarsu/coronaexample/ui/SplashActivity.kt
@@ -1,17 +1,19 @@
-package org.ucarsu.coronaexample
+package org.ucarsu.coronaexample.ui
 
 import android.os.Bundle
 import android.os.Handler
 import presentation.activity.BaseActivity
 import navigation.navigations.DefaultNavigationController
 import navigation.navigations.NavigationController
+import org.ucarsu.coronaexample.R
 import java.lang.ref.WeakReference
 
 class SplashActivity : BaseActivity() {
 
     private lateinit var navigationController: NavigationController
 
-    override fun getLayoutRes(): Int = R.layout.activity_splash
+    override fun getLayoutRes(): Int =
+        R.layout.activity_splash
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
